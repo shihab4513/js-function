9dd8447446923cf489d4a0803f37f703960f3ec0
shihab4513 <shihab4513@gmail.com>
2024-02-03 16:24:16 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,16 @@
+const number=45;
+function fanOffkor()
+{
+    console.log("bosa theke uthe dara");
+    console.log("click the switch to off the fan");
+}
+
+// fanOffkor();
+
+function brushTeeth()
+{
+    console.log('Pick  up the brush');
+}
+
+// call the function
+brushTeeth();
